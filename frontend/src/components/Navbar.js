32e8b2bc83400ebe94279b7fc506872210995781
f1x3d49 <davidnicoara28@gmail.com>
2023-07-02 16:42:03 +0300

    chore: working on the slider component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Import Assets
 import logo from "../assets/shared/logo.svg";
+import { ReactComponent as Hamburger } from "../assets/shared/icon-hamburger.svg";
+
+// Import Router Link
 import { Link } from "react-router-dom";
 
+// Import Slide Over
+import SliderComponent from "./SliderComponent";
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <nav className="absolute top-0 w-full bg-red-500 flex justify-between">
+    <nav className="absolute top-0 w-full flex shrink-0 justify-between items-center mb:px-[24px] mb:pt-[24px]">
       <img
         src={logo}
         alt="Space Logo"
         className="mb:w-10 mb:h-10 tb:w-12 tb:h-12"
       />
-      <ul className="flex items-center justify-center">
+      {/* Tablet and Desktop Navigation */}
+      <ul className="hidden tb:flex items-center justify-center text-white">
         <li>
           <Link to="/" className="flex gap-1">
             <h1>00</h1>
@@ -36,6 +46,11 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+      {/* Mobile Menu */}
+      <button onClick={() => setOpen(true)}>
+        <Hamburger className="w-[24px] h-[21px]" />
+      </button>
+      <SliderComponent open={open} setOpen={setOpen} />
     </nav>
   );
 };
