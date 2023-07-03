6c54568864ffc2a29a6765d2325ac3225fe1887b
f1x3d49 <davidnicoara28@gmail.com>
2023-07-03 14:48:11 +0300

    chore: finished building the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -19,29 +19,31 @@ const Navbar = () => {
         alt="Space Logo"
         className="mb:w-10 mb:h-10 mb:ml-[24px] mb:mt-[24px] tb:w-12 tb:h-12"
       />
+      {/* Desktop Navbar Line */}
+      <div className="mb:hidden dt:inline-block h-[1px] w-[473px] bg-primary3 opacity-25 absolute left-[180px] z-10 top-[70px]"></div>
       {/* Tablet and Desktop Navigation */}
-      <ul className="hidden h-[96px] tb:w-[450px] dt:w-[830px] tb:flex tb:order-last items-center justify-center tb:gap-10 tb:py-[40px] bg-[rgba(255,255,255,0.04)] backdrop text-primary3 font-barlowcondensed text-[14px] uppercase tracking-[2.362px]">
+      <ul className="hidden h-[96px] tb:w-[450px] dt:w-[830px] tb:flex tb:order-last items-center justify-center dt:mt-[24px] tb:gap-10 tb:py-[40px] shadow-2xl shadow-primary1 bg-[rgba(255,255,255,0.04)] backdrop text-primary3 font-barlowcondensed text-[14px] dt:text-[16px] uppercase tracking-[2.362px]">
         <li>
           <NavLink to="/" className="flex gap-3 relative bar">
-            <h1 className="tb:hidden dt:inline-block">00</h1>
+            <h1 className="tb:hidden dt:inline-block font-bold">00</h1>
             <p>Home</p>
           </NavLink>
         </li>
         <li>
           <NavLink to="/destination" className="flex gap-3 relative bar">
-            <h1 className="tb:hidden dt:inline-block">01</h1>
+            <h1 className="tb:hidden dt:inline-block font-bold">01</h1>
             <p>Destination</p>
           </NavLink>
         </li>
         <li>
           <NavLink to="/crew" className="flex gap-3 relative bar">
-            <h1 className="tb:hidden dt:inline-block">02</h1>
+            <h1 className="tb:hidden dt:inline-block font-bold">02</h1>
             <p>Crew</p>
           </NavLink>
         </li>
         <li>
           <NavLink to="/technology" className="flex gap-3 relative bar">
-            <h1 className="tb:hidden dt:inline-block">03</h1>
+            <h1 className="tb:hidden dt:inline-block font-bold">03</h1>
             <p>Technology</p>
           </NavLink>
         </li>
