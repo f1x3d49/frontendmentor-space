a1044a7ef3a807b0f7004400d6bf366004c1501b
f1x3d49 <davidnicoara28@gmail.com>
2023-07-05 19:20:27 +0300

    chore: finshed the crew page

--- frontend/src/components/CircleTabComponent.js
+++ frontend/src/components/CircleTabComponent.js
@@ -6,13 +6,15 @@ import { crewData } from "../data/crewData";
 // Import HeadlessUI tab Component
 import { Tab } from "@headlessui/react";
 
-const CircleTabComponent = () => {
+const CircleTabComponent = ({ selectedIndex, setSelectedIndex }) => {
   return (
     <Tab.Group
+      selectedIndex={selectedIndex}
+      onChange={setSelectedIndex}
       as="div"
-      className="flex flex-col justify-center items-center gap-[24px]"
+      className="dt:w-[680px] flex flex-col justify-center items-center dt:items-start gap-[24px] dt:ml-[165px]"
     >
-      <Tab.List className="flex items-center justify-between gap-[20px] tb:gap-[27px] dt:gap-[35px] relative tb:mt-[20px] dt:mt-0">
+      <Tab.List className="flex items-center justify-between gap-[20px] tb:gap-[27px] dt:gap-[35px] relative tb:mt-[20px] dt:mt-0 tb:order-last">
         <Tab as={Fragment}>
           {({ selected }) => (
             <button
@@ -55,11 +57,17 @@ const CircleTabComponent = () => {
           return (
             <Tab.Panel
               key={key}
-              className="w-full flex flex-col justify-center items-center dt:items-start tb:gap-[36px] text-primary3"
+              className="w-full flex flex-col justify-center items-center dt:items-start tb:gap-[8px] text-primary3"
             >
-              <h2>{data.role}</h2>
-              <h1>{data.name}</h1>
-              <p>{data.bio}</p>
+              <h2 className="opacity-50 text-[16px] tb:text-[24px] dt:text-[32px] font-bellefair uppercase mb-[8px] tb:mb-0 dt:mb-[8px]">
+                {data.role}
+              </h2>
+              <h1 className="text-[24px] tb:text-[40px] dt:text-[56px]  font-bellefair uppercase mb-[16px] tb:mb-0 dt:mb-[16px]">
+                {data.name}
+              </h1>
+              <p className="text-[15px] tb:text-[16px] dt:text-[18px] mx-[24px] tb:mx-[155px] dt:mx-0 text-center dt:text-left text-primary2 font-barlow leading-[25px] tb:leading-[28px] dt:leading-[32px] mb-[104px] tb:mb-0 dt:w-[444px]">
+                {data.bio}
+              </p>
             </Tab.Panel>
           );
         })}
--- frontend/src/pages/Crew.js
+++ frontend/src/pages/Crew.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// Astronaut Images
 import douglas from "../assets/crew/image-douglas-hurley.png";
+import anousheh from "../assets/crew/image-anousheh-ansari.png";
+import mark from "../assets/crew/image-mark-shuttleworth.png";
+import victor from "../assets/crew/image-victor-glover.png";
+
 import CircleTabComponent from "../components/CircleTabComponent";
 
 const Crew = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [imgSource, setImgSource] = useState(douglas);
+
+  useEffect(() => {
+    if (selectedIndex === 0) setImgSource(douglas);
+    else if (selectedIndex === 1) setImgSource(mark);
+    else if (selectedIndex === 2) setImgSource(victor);
+    else if (selectedIndex === 3) setImgSource(anousheh);
+  }, [selectedIndex, setSelectedIndex]);
+
   return (
-    <section className="bg-fixed bg-no-repeat bg-cover bg-bgCrewMobile tb:bg-bgCrewTablet dt:bg-bgCrewDesktop w-full h-screen">
-      <div className="w-full h-full flex flex-col justify-start items-center gap-[32px] pt-[89px] tb:pt-[136px] dt:pt-[212px]">
+    <section className="bg-fixed bg-no-repeat bg-cover bg-bgCrewMobile tb:bg-bgCrewTablet dt:bg-bgCrewDesktop w-full h-full dt:h-screen">
+      <div className="w-full h-full flex flex-col justify-start items-center gap-[32px]  pt-[89px] tb:pt-[136px] dt:pt-[212px]">
         {/* Header Text */}
         <div className="flex justify-center items-center gap-[16px] tb:self-start tb:ml-[38.5px] dt:ml-[167px]">
           <p className="text-primary3 opacity-25 text-[16px] tb:text-[20px] dt:text-[28px] font-barlowcondensed font-bold tracking-[2.7px] tb:tracking-[3.375px] dt:tracking-[4.725px]">
@@ -18,11 +34,18 @@ const Crew = () => {
         {/* Astronaut Image and Round Tabs */}
         <div className="flex flex-col dt:flex-row items-center justify-center dt:justify-between dt:w-full gap-[32px] ">
           {/* Astronaut Image and Mobile Line */}
-          <div className="w-[327px] flex items-center justify-center border-b-[1px] border-[#383B4B] tb:border-0">
-            <img src={douglas} alt="astronaut" className="w-1/2" />
+          <div className="w-[327px] tb:w-auto flex items-center dt:mr-[137px] justify-center border-b-[1px] border-[#383B4B] tb:border-0 tb:order-last">
+            <img
+              src={imgSource}
+              alt="astronaut"
+              className="w-1/2 tb:w-auto dt:absolute bottom-0 right-10"
+            />
           </div>
           {/* Circle Tab */}
-          <CircleTabComponent />
+          <CircleTabComponent
+            selectedIndex={selectedIndex}
+            setSelectedIndex={setSelectedIndex}
+          />
         </div>
       </div>
     </section>
