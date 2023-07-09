1ffe490216e50fd85ebaf611c765102f2700a249
f1x3d49 <davidnicoara28@gmail.com>
2023-07-09 18:47:38 +0300

    chore: working on the technology tab

--- frontend/src/components/NumberTabComponent.js
+++ frontend/src/components/NumberTabComponent.js
@@ -10,13 +10,13 @@ const NumberTabComponent = () => {
   return (
     <Tab.Group
       as="div"
-      className="flex flex-col justify-center items-center gap-[24px]"
+      className="flex flex-col dt:flex-row justify-center dt:items-start items-center gap-[24px] dt:gap-[80px] dt:ml-[165px]"
     >
-      <Tab.List className="flex items-center justify-between gap-[20px] tb:gap-[27px] dt:gap-[35px] tb:mt-[20px] dt:mt-0">
+      <Tab.List className="flex dt:flex-col items-center justify-between gap-[20px] tb:gap-[27px] dt:gap-[35px] tb:mt-[20px] dt:mt-0">
         <Tab as={Fragment}>
           {({ selected }) => (
             <button
-              className={`w-[40px] h-[40px] tb:w-[60px] tb:h-[60px] dt:w-[80px] dt:h-[80px] rounded-full  border-[1px] border-primary3 text-center font-bellefair text-[16px] tracking-[1px] tb:text-[24px] tb:tracking-[1.5px] dt:text-[32px] dt:tracking-[2px] ${
+              className={`w-[40px] h-[40px] tb:w-[60px] tb:h-[60px] dt:w-[80px] dt:h-[80px] rounded-full  border-[1px] border-primary3 flex items-center justify-center font-bellefair text-[16px] tracking-[1px] tb:text-[24px] tb:tracking-[1.5px] dt:text-[32px] dt:tracking-[2px] ${
                 selected
                   ? " bg-primary3 border-opacity-100 text-primary1"
                   : "border-opacity-25 text-primary3"
@@ -29,7 +29,7 @@ const NumberTabComponent = () => {
         <Tab as={Fragment}>
           {({ selected }) => (
             <button
-              className={`w-[40px] h-[40px] tb:w-[60px] tb:h-[60px] dt:w-[80px] dt:h-[80px] rounded-full border-[1px] border-primary3 text-center font-bellefair text-[16px] tracking-[1px] tb:text-[24px] tb:tracking-[1.5px] dt:text-[32px] dt:tracking-[2px] ${
+              className={`w-[40px] h-[40px] tb:w-[60px] tb:h-[60px] dt:w-[80px] dt:h-[80px] rounded-full border-[1px] border-primary3 flex items-center justify-center font-bellefair text-[16px] tracking-[1px] tb:text-[24px] tb:tracking-[1.5px] dt:text-[32px] dt:tracking-[2px] ${
                 selected
                   ? "bg-primary3 border-opacity-100 text-primary1"
                   : "border-opacity-25 text-primary3"
@@ -42,7 +42,7 @@ const NumberTabComponent = () => {
         <Tab as={Fragment}>
           {({ selected }) => (
             <button
-              className={`w-[40px] h-[40px] tb:w-[60px] tb:h-[60px] dt:w-[80px] dt:h-[80px] rounded-full border-[1px] border-primary3 text-center font-bellefair text-[16px] tracking-[1px] tb:text-[24px] tb:tracking-[1.5px] dt:text-[32px] dt:tracking-[2px] ${
+              className={`w-[40px] h-[40px] tb:w-[60px] tb:h-[60px] dt:w-[80px] dt:h-[80px] rounded-full border-[1px] border-primary3 flex items-center justify-center font-bellefair text-[16px] tracking-[1px] tb:text-[24px] tb:tracking-[1.5px] dt:text-[32px] dt:tracking-[2px] ${
                 selected
                   ? "bg-primary3 border-opacity-100 text-primary1"
                   : "border-opacity-25 text-primary3"
@@ -58,15 +58,15 @@ const NumberTabComponent = () => {
           return (
             <Tab.Panel
               key={key}
-              className="w-full flex flex-col justify-center items-center dt:items-start tb:gap-[8px]"
+              className="w-full flex flex-col dt:w-[470px] justify-center items-center dt:items-start tb:gap-[8px]"
             >
-              <h2 className="text-primary2 uppercase text-center text-[14px] tb:text-[16px] tracking-[2.362px] tb:tracking-[2.7px] font-barlowcondensed">
+              <h2 className="text-primary2 uppercase text-center dt:text-left text-[14px] tb:text-[16px] tracking-[2.362px] tb:tracking-[2.7px] font-barlowcondensed">
                 The Terminology...
               </h2>
-              <h1 className="text-primary3 text-center uppercase text-[24px] tb:text-[40px] dt:text-[56px] font-bellefair">
+              <h1 className="text-primary3 text-center dt:text-left uppercase text-[24px] tb:text-[40px] dt:text-[56px] font-bellefair">
                 {data.name}
               </h1>
-              <p className="text-primary2 text-center text-[15px] tb:text-[16px] dt:text-[18px] leading-[25px] tb:leading-[28px] dt:leading-[32px] font-barlow">
+              <p className="text-primary2 text-center dt:text-left text-[15px] tb:text-[16px] dt:text-[18px] leading-[25px] tb:leading-[28px] dt:leading-[32px] font-barlow mx-[24px] tb:mx-[155px] dt:mx-0">
                 {data.description}
               </p>
             </Tab.Panel>
--- frontend/src/pages/Technology.js
+++ frontend/src/pages/Technology.js
@@ -3,11 +3,20 @@ import React, { useState, useEffect } from "react";
 // Technology Images
 import vehiclelandscape from "../assets/technology/image-launch-vehicle-landscape.jpg";
 import vehicleportrait from "../assets/technology/image-launch-vehicle-portrait.jpg";
+import capsulelandscape from "../assets/technology/image-space-capsule-landscape.jpg";
+import capsuleportrait from "../assets/technology/image-space-capsule-portrait.jpg";
+import spaceportportrait from "../assets/technology/image-spaceport-portrait.jpg";
+import spaceportlandscape from "../assets/technology/image-spaceport-landscape.jpg";
 import NumberTabComponent from "../components/NumberTabComponent";
 
 const Technology = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [portraitImgSource, setPortraitImgSource] = useState(vehicleportrait);
+  const [landscapeImgSrouce, setLandscapeImgSource] =
+    useState(vehiclelandscape);
+
   return (
-    <section className="bg-fixed bg-cover bg-no-repeat bg-bgTechnologyMobile tb:bg-bgTechnologyTablet dt:bg-bgTechnologyDesktop w-full h-full">
+    <section className="bg-fixed bg-cover bg-no-repeat bg-bgTechnologyMobile tb:bg-bgTechnologyTablet dt:bg-bgTechnologyDesktop w-full h-screen">
       <div className="w-full h-full flex flex-col justify-start items-center gap-[32px] pt-[89px] tb:pt-[136px] dt:pt-[212px]">
         {/* Header Text */}
         <div className="flex justify-center items-center gap-[18px] tb:self-start tb:ml-[38.5px] dt:ml-[167px]">
@@ -19,11 +28,11 @@ const Technology = () => {
           </h1>
         </div>
         {/* Tech Images and Tabs */}
-        <div className="flex flex-col dt:flex-row items-center justify-center dt:justify-between dt:w-full gap-[32px]">
-          <picture className="w-screen h-full dt:w-[550px] dt:h-[550px] dt:ml-[119px]">
+        <div className="flex flex-col dt:flex-row items-center justify-center dt:justify-between dt:w-full gap-[32px] dt:mt-[137px]">
+          <picture className="h-full w-full dt:order-last">
             <source media="(min-width: 300px)" srcSet={vehiclelandscape} />
-            <source media="(min-width: 1400px)" srcSet={vehicleportrait} />
-            <img src={vehicleportrait} alt="vehicle" />
+            <source media="(min-width: 1300px)" srcSet={vehicleportrait} />
+            <img src={vehiclelandscape} alt="vehicle" />
           </picture>
           <NumberTabComponent />
         </div>
